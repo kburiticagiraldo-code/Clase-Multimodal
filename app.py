9866ceec689336e915a8245aaeeebae7448d5261
kburiticagiraldo-code <kburiticagiraldo@gmail.com>
2026-08-20 13:42:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,3 +25,10 @@ with col1:
 
 with col2:
     st.subheader("Esta es la segunda columna")
+    modo = st.radio("Que modalidad es la principal es tu interfaz", ('Visual', 'auditiva', 'Táctil'))
+    if modo == 'Visual':
+        st.write('La vista es fundamental para tu interfaz')
+    if modo == 'auditiva':
+        st.write('La audición es fundamental para tu interfaz')
+    if modo == 'Táctil':
+        st.write('El tacto es fundamental para tu interfaz')
