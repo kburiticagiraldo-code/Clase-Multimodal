f922f9c27564403b4369380e17de0ff11f4109d7
kburiticagiraldo-code <kburiticagiraldo@gmail.com>
2026-08-20 13:45:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ st.title(" Mi primera página")
 st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write("Facilmente puedo realizar backend y frontend.")
 image = Image.open('Dragon.jpg')
-st.image(image, caption='Interfaces multimodales')
+st.image(image, caption='Dragon')
 
 
 texto = st.text_input('Escribe algo', 'Este es mi texto')
