86c72bc74aa9069fa7ed617987226891af6184a7
kburiticagiraldo-code <kburiticagiraldo@gmail.com>
2026-08-20 13:19:58 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
+from PIL import Image
 
-st.title("Hola !!! mi nombre Kevin")
+st.title(" Mi primera página")
+
+st.header(En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write(Facilmente puedo realizar backend y frontend.")
+image = Image.open('dragon
