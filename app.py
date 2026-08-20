69a63aea87b40720cd323b03668508a4bedc69df
kburiticagiraldo-code <kburiticagiraldo@gmail.com>
2026-08-20 13:23:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 st.title(" Mi primera página")
 
-st.header(En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write(Facilmente puedo realizar backend y frontend.")
 image = Image.open('Dragon.jpg')
 st.image(image, caption='Interfaces multimodales')
