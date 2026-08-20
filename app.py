29f58cf0de3b60cbe95441207f5c038c055d9dad
kburiticagiraldo-code <kburiticagiraldo@gmail.com>
2026-08-20 13:22:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ st.title(" Mi primera página")
 
 st.header(En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
 st.write(Facilmente puedo realizar backend y frontend.")
-image = Image.open('dragon
+image = Image.open('Dragon.jpg')
+st.image(image, caption='Interfaces multimodales')
